48a8e407760cbaa8f5509219045622ae93f6a603
bloodybuttock <jurukuncicpm@gmail.com>
2021-02-01 20:00:41 +0700

    deployyy

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(__dirname + '/dist/neza-yasser'));
 
 app.get('/*', (req, res) =>
