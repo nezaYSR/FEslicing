ed782e8e4e3ead95c7fc9a140ec41e448e0c4101
bloodybuttock <jurukuncicpm@gmail.com>
2021-02-01 18:31:49 +0700

    deployyy

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.static('./dist/neza-yasser'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/neza-yasser/'}),
+    {console.log('tatata');
+    res.sendFile('index.html', {root: 'dist/neza-yasser/'})}
 );
 
 app.listen(process.env.PORT || 8080);
