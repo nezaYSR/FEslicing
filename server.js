0f1343e0f1c47e2681a7fb90e6137a9bc5ca1217
bloodybuttock <jurukuncicpm@gmail.com>
2021-02-01 18:08:29 +0700

    deployyy

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const path=require('path')
-const app =express()
-app.use(express.static(__dirname+'/dist/neza-yasser'))
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/neza-yasser/index.html'))
-})
-app.listen(process.env.PORT || 8080)
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/neza-yasser'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/neza-yasser/'}),
+);
+
+app.listen(process.env.PORT || 8080);
