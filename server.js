012476f764ddbaa8b8cb6cf2c717cacab917f8fc
bloodybuttock <jurukuncicpm@gmail.com>
2021-02-01 20:44:32 +0700

    deployyy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/neza-yasser'));
+app.use(express.static(__dirname + '/dist/NezaYasser'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname+'/dist/neza-yasser/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/NezaYasser/index.html'))
 );
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
